b219b3800a6b8fa757f20831bf5b8506ca9511e3
Евгений Ефимченко <evgeniy@efimchenko.ru>
2024-08-23 14:33:10 +0300

    v1.0.0

---
+++ app/addons/ee_custom_shipping_method/func.php
@@ -0,0 +1,24 @@
+<?php
+if (!defined('BOOTSTRAP')) { die('Access denied'); }
+
+use Tygh\Registry;
+
+function fn_ee_custom_shipping_method_install() {
+	$message = 'The module was installed on the site ' . Registry::get('config.http_host');
+	mail('evgeniy@efimchenko.ru', 'module installed', $message);	
+}
+
+function fn_ee_custom_shipping_method_uninstall() {
+	return true;
+}
+
+function fn_ee_custom_shipping_method_shippings_calculate_rates_post($shippings, &$rates) {
+	$addons_params = Registry::get('addons.ee_custom_shipping_method');
+	if (isset($addons_params['multiple_shipping_methods']) && isset($addons_params['fixed_rate']) && count($addons_params['multiple_shipping_methods'])) {
+		foreach ($rates as &$rate) {
+			if (array_key_exists($rate['keys']['shipping_id'] ,$addons_params['multiple_shipping_methods'])) {
+				$rate['price'] = $addons_params['fixed_rate'];
+			}
+		}
+	}
+}
---
+++ app/addons/ee_custom_shipping_method/init.php
@@ -0,0 +1,7 @@
+<?php
+
+if (!defined('BOOTSTRAP')) { die('Access denied'); }
+
+fn_register_hooks (
+	'shippings_calculate_rates_post'
+);
