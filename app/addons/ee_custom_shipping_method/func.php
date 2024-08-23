6ca7210b0702899ce0213a499b62f28c48e7c1d2
Евгений Ефимченко <evgeniy@efimchenko.ru>
2024-08-23 14:58:15 +0300

    fix bug

--- app/addons/ee_custom_shipping_method/func.php
+++ app/addons/ee_custom_shipping_method/func.php
@@ -14,7 +14,8 @@ function fn_ee_custom_shipping_method_uninstall() {
 
 function fn_ee_custom_shipping_method_shippings_calculate_rates_post($shippings, &$rates) {
 	$addons_params = Registry::get('addons.ee_custom_shipping_method');
-	if (isset($addons_params['multiple_shipping_methods']) && isset($addons_params['fixed_rate']) && count($addons_params['multiple_shipping_methods'])) {
+	// fn_print_die($addons_params);
+	if (isset($addons_params['multiple_shipping_methods']) && isset($addons_params['fixed_rate']) && is_array($addons_params['multiple_shipping_methods']) && count($addons_params['multiple_shipping_methods'])) {
 		foreach ($rates as &$rate) {
 			if (array_key_exists($rate['keys']['shipping_id'] ,$addons_params['multiple_shipping_methods'])) {
 				$rate['price'] = $addons_params['fixed_rate'];
